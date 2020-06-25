347e80a76645b3ae97664b64c34d5d7da2d90384
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-24 21:11:17 -0300

    MOntando layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Main from './pages/Main';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+      <Main></Main>
   );
 }
 
